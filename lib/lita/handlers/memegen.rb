c5c4c3b20f45fd83162cf179a60900fa67bb435e
Joel Cogen <joel@redilio.us>
2014-08-13 19:20:43 +0200

    Port to imgflip Memegenerator sucks mmmkay

--- lib/lita/handlers/memegen.rb
+++ lib/lita/handlers/memegen.rb
@@ -9,138 +9,87 @@ module Lita
         config.username = nil
         config.password = nil
       end
-      
+
       route %r{(Y U NO) (.+)}i,                                    :meme_y_u_no,           help: { "Y U NO..." => "generates Y U NO meme"}
-      route %r{(I DON'?T ALWAYS .*) (BUT WHEN I DO,? .*)}i,        :meme_i_dont_always,    help: { "I DON'T ALWAYS .. BUT WHEN I DO, .." => "generates I DON'T ALWAY meme"} 
-      route %r{(.*)(O\s?RLY\??.*)}i,                               :meme_orly,             help: { "..O RLY.." => "generates O RLY meme" }              
+      route %r{(I DON'?T ALWAYS .*) (BUT WHEN I DO,? .*)}i,        :meme_i_dont_always,    help: { "I DON'T ALWAYS .. BUT WHEN I DO, .." => "generates I DON'T ALWAY meme"}
       route %r{(.*)(SUCCESS|NAILED IT.*)},                         :meme_success,          help: { "..SUCCESS.." => "(case sensitive) generates SUCCESS meme", "..NAILED IT.." => "generates NAILED IT meme" }
       route %r{(.*) (ALL the .*)},                                 :meme_all_the,          help: { "ALL the.." => "(case sensitive) generates ALL the <things> meme" }
       route %r{(.*) (\w+\sTOO DAMN .*)}i,                          :meme_too_damn,         help: { "TOO DAMN.." => "generates TOO DAMN meme" }
-      route %r{(GOOD NEWS EVERYONE[,.!]?) (.*)}i,                  :meme_good_news,        help: { "GOOD NEWS EVERYONE.." => "generates GOOD NEWS EVERYONE meme" } 
       route %r{(NOT SURE IF .*) (OR .*)}i,                         :meme_not_sure_if,      help: { "NOT SURE IF.. OR.." => "generates NOT SURE IF .. OR meme" }
-      route %r{(YO DAWG .*) (SO .*)}i,                             :meme_yo_dawg,          help: { "YO DAWG.." => "generates YO DAWG meme" }   
-      route %r{(ALL YOUR .*) (ARE BELONG TO US)}i,                 :meme_are_belong,       help: { "ALL YOUR.. ARE BELONG TO US" => "generates ALL YOUR.. ARE BELONG TO US meme" }       
-      route %r{(.*) (FUCK YOU)}i,                                  :meme_fuck_you,         help: { "..FUCK YOU" => "generates ..FUCK YOU meme" }     
-      route %r{(.*) (You'?re gonna have a bad time)}i,             :meme_bad_time,         help: { ".. You're going have a bad time" => "generates You're gonna have a bad time meme" }     
-      route %r{(one does not simply) (.*)}i,                       :meme_simply,           help: { "one does not simply.." => "generates one does not simply.. meme" }   
-      route %r{(grumpy cat) (.*),(.*)}i,                           :meme_grumpy_cat,       help: { "grumpy cat .. , .." => "generates grumpy cat .. , .. meme" }       
-      route %r{(it looks like you're|it looks like you) (.*)}i,    :meme_looks_like,       help: { "it looks like you're .." => "generates it looks like you're .. meme", "it looks like you.." => "(case insensitive) generates it looks like you.. meme" }       
-      route %r{(AM I THE ONLY ONE AROUND HERE) (.*)}i,             :meme_am_i_only,        help: { "AM I THE ONLY ONE AROUND HERE.." => "generates AM I THE ONLY ONE AROUND HERE.. meme" }     
-      route %r{(.*)(NOT IMPRESSED*)}i,                             :meme_not_impressed,    help: { "..NOT IMPRESSED" => "generates ..NOT IMPRESSED meme" }         
-      route %r{(PREPARE YOURSELF) (.*)}i,                          :meme_prepare_yourself, help: { "PREPARE YOURSELF.." => "generates PREPARE YOURSELF.. meme" }             
-      route %r{(WHAT IF I TOLD YOU) (.*)}i,                        :meme_what_if_i,        help: { "WHAT IF I TOLD YOU.." => "generates WHAT IF I TOLD YOU.. meme" }     
-      route %r{(.*) (BETTER DRINK MY OWN PISS)}i,                  :meme_better_drink,     help: { "..BETTER DRINK MY OWN PISS" => "generates ..BETTER DRINK MY OWN PISS meme" }
-      route %r{^khanify (.*)}i,                                    :meme_khanify,          help: { "khanify ..." => "generates khan meme" }
-
+      route %r{(YO DAWG .*) (SO .*)}i,                             :meme_yo_dawg,          help: { "YO DAWG.." => "generates YO DAWG meme" }
+      route %r{(.*) (You'?re gonna have a bad time)}i,             :meme_bad_time,         help: { ".. You're going have a bad time" => "generates You're gonna have a bad time meme" }
+      route %r{(one does not simply) (.*)}i,                       :meme_simply,           help: { "one does not simply.." => "generates one does not simply.. meme" }
+      route %r{(grumpy cat) (.*),(.*)}i,                           :meme_grumpy_cat,       help: { "grumpy cat .. , .." => "generates grumpy cat .. , .. meme" }
+      route %r{(AM I THE ONLY ONE AROUND HERE) (.*)}i,             :meme_am_i_only,        help: { "AM I THE ONLY ONE AROUND HERE.." => "generates AM I THE ONLY ONE AROUND HERE.. meme" }
+      route %r{(BRACE YOURSEL(F|VES)) (.*)}i,                      :meme_brace_yourselves, help: { "BRACE YOURSELVES.." => "generates BRACE YOURSELVES.. meme" }
+      route %r{(WHAT IF I TOLD YOU) (.*)}i,                        :meme_what_if_i,        help: { "WHAT IF I TOLD YOU.." => "generates WHAT IF I TOLD YOU.. meme" }
 
       def meme_y_u_no(response)
-        generate_meme(response, 2, 166088)
+        generate_meme(response, 61527)
       end
 
       def meme_i_dont_always(response)
-        generate_meme(response, 74, 2485)
-      end
-
-      def meme_orly(response)
-        generate_meme(response, 920, 117049)
+        generate_meme(response, 61532)
       end
 
       def meme_success(response)
-        generate_meme(response, 121, 1031)
+        generate_meme(response, 61544)
       end
 
       def meme_all_the(response)
-        generate_meme(response, 6013, 1121885)
+        generate_meme(response, 61533)
       end
 
       def meme_too_damn(response)
-        generate_meme(response, 998, 203665)
-      end
-
-      def meme_good_news(response)
-        generate_meme(response, 1591, 112464)
+        generate_meme(response, 61580)
       end
 
       def meme_not_sure_if(response)
-        generate_meme(response, 305, 84688)
+        generate_meme(response, 61520)
       end
 
       def meme_yo_dawg(response)
-        generate_meme(response, 79, 108785)
-      end
-
-      def meme_are_belong(response)
-        generate_meme(response, 349058, 2079825)
-      end
-
-      def meme_fuck_you(response)
-        generate_meme(response, 1189472, 5044147)
+        generate_meme(response, 101716)
       end
 
       def meme_bad_time(response)
-        generate_meme(response, 825296, 3786537)
+        generate_meme(response, 100951)
       end
 
       def meme_simply(response)
-        generate_meme(response, 274947, 1865027)
+        generate_meme(response, 61579)
       end
 
       def meme_grumpy_cat(response)
-        generate_meme(response, 1590955, 6541210)
-      end
-
-      def meme_looks_like(response)
-        generate_meme(response, 20469, 1159769)
+        generate_meme(response, 405658)
       end
 
       def meme_am_i_only(response)
-        generate_meme(response, 953639, 4240352)
+        generate_meme(response, 259680)
       end
 
-      def meme_not_impressed(response)
-        generate_meme(response, 1420809, 5883168)
-      end
-
-      def meme_prepare_yourself(response)
-        generate_meme(response, 414926, 2295701)
+      def meme_brace_yourselves(response)
+        generate_meme(response, 61546)
       end
 
       def meme_what_if_i(response)
-        generate_meme(response, 1118843, 4796874)
+        generate_meme(response, 100947)
       end
 
-      def meme_better_drink(response)
-        generate_meme(response, 92, 89714)
-      end
+      private
 
-      def meme_khanify(response)
-        generate_meme(response, 6443, 1123022, line1: "", line2: khanify(response.matches[0][0]))
-      end
-
-
-      private 
-
-      def khanify(phrase)
-        shouty_phrase = phrase.upcase
-        last_vowel_index = shouty_phrase.rindex(/[AEIOU]/) || -1 # default to final consonant
-        last_vowel = shouty_phrase[last_vowel_index] 
-        "#{shouty_phrase[0..last_vowel_index]}#{10.times.map{ last_vowel }.join}#{shouty_phrase[last_vowel_index..-1]}!!!!"
-      end
-
-      def generate_meme(response, generator_id, image_id, line1: nil, line2: nil)
+      def generate_meme(response, template_id, line1: nil, line2: nil)
         return if Lita.config.handlers.memegen.command_only && !response.message.command?
-        
+
         line1 ||= response.matches[0][0]
         line2 ||= response.matches[0][1]
         return if Lita.config.handlers.memegen.username.nil? || Lita.config.handlers.memegen.password.nil?
-        
-        http_resp = http.get(
-          'http://version1.api.memegenerator.net/Instance_Create',
+
+        http_resp = http.post(
+          'https://api.imgflip.com/caption_image',
           username: Lita.config.handlers.memegen.username,
           password: Lita.config.handlers.memegen.password,
-          languageCode: 'en',
-          generatorID: generator_id,
-          imageID: image_id,
+          template_id: template_id,
           text0: line1,
           text1: line2
           )
@@ -149,7 +98,7 @@ module Lita
           data = MultiJson.load(http_resp.body)
 
           if data['success']
-            response.reply data['result']['instanceImageUrl']
+            response.reply data['data']['url']
           else
             Lita.logger.error "#{self.class}: Unable to generate a meme image: #{data.inspect}"
             response.reply "Error: Unable to generate a meme image"
--- spec/lita/handlers/memegen_spec.rb
+++ spec/lita/handlers/memegen_spec.rb
@@ -3,12 +3,11 @@ require "spec_helper"
 describe Lita::Handlers::Memegen, lita_handler: true do
   it { routes_command("Y U NO BLAH").to(:meme_y_u_no) }
   it { routes_command("i don't always fart but when i do, i do it loud").to(:meme_i_dont_always) }
-  it { routes_command("blah o rly?").to(:meme_orly) }
-  it { routes_command("khanify blah").to(:meme_khanify) }
+  it { routes_command("blah you're gonna have a bad time").to(:meme_bad_time) }
+  it { routes_command("what if i told you blah").to(:meme_what_if_i) }
 
   it "sets the username and password to nil by default" do
     expect(Lita.config.handlers.memegen.username).to be_nil
     expect(Lita.config.handlers.memegen.password).to be_nil
   end
-
 end
